79d237206f8b2bddc472727eb1a8bab4cd18c2d8
OmarNoob27 <omarahmed9113@gmail.com>
2022-08-10 13:35:03 +0200

    update my config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -94,9 +94,9 @@ end
 #    printf  '%s\n' 'abcdefghijklmnopqrstuvwxyz'  ' ' | lolcat
 #end
 
-#function commits
-#    git log --author="$argv" --format=format:%ad --date=short | uniq -c | awk '{print $1}' | spark | lolcat
-#end
+function commits
+    git log --author="$argv" --format=format:%ad --date=short | uniq -c | awk '{print $1}' | spark | lolcat
+end
 
 # Functions needed for !! and !$
 #function __history_previous_command
