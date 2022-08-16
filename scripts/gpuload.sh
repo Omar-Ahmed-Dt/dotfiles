ec2c375267c0534efb4a74fc9882e33cffeb6b54
OmarNoob27 <omarahmed9113@gmail.com>
2022-08-16 21:23:21 +0200

    update my config

--- scripts/gpuload.sh
+++ scripts/gpuload.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 load=$(nvidia-settings -q all | grep -m1 'UsedDedicatedGPUMemory' | awk -F: '{print $4}' | awk '{print ($0+0)}')
-echo $load MB
+#echo $load MB
+utilize=$(nvidia-smi --format=nounits,csv,noheader --query-gpu=utilization.gpu | xargs echo)
+echo $utilize% / $load MB
--- scripts/resources.sh
+++ scripts/resources.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-OPT=$(echo -e "htop\nbashtop\nbpytop\ngotop\nGPU" | dmenu  -fn 'NotoMonoRegular:bold:pixelsize=18' -p "Choose action: ")
+OPT=$(echo -e "htop\nbashtop\nbpytop\ngotop\nGPU_1\nGPU_2" | dmenu  -fn 'NotoMonoRegular:bold:pixelsize=18' -p "Choose action: ")
 case $OPT in
 	htop )  kitty -e htop ;;
 	bashtop ) kitty -e bashtop ;;
 	bpytop )  kitty -e bpytop ;; 
 	gotop )  kitty -e gotop ;; 
-	GPU )     kitty -e nvtop ;; 
+	GPU_1)     kitty -e nvtop ;; 
+	GPU_2 )     kitty -e nvitop ;; 
 	*) ;;
 esac
 
