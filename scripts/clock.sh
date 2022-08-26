1033c07a63e145b01dbed034f319bfff89e078e4
OmarNoob27 <omarahmed9113@gmail.com>
2022-08-26 21:11:33 +0200

    update my dotfiles

--- scripts/clock.sh
+++ scripts/clock.sh
@@ -2,7 +2,9 @@
 
 #date '+%Y %b %d (%a) %I:%M%p'
 #date '+%d-%m-%y (%a) %I:%M%p'
-date '+%a %d-%m'
+#date '+%a %d-%m'
+
+date +"%a, %B %d"| sed 's/  / /g'
 
 #case $BLOCK_BUTTON in
 #	1) pgrep -x dunst >/dev/null && notify-send "This Month" "$(cal --color=always | sedTERMINAL"s/..7m/<b><span color=\"red\">/;s/..27m/<\/span><\/b>/")" && notify-send "Appointments" "$(calcurse -D ~/.config/calcurse -d7)" ;;
