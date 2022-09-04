c0acb68a0e10b3f3a2e7e15b79306968490aad73
OmarNoob27 <omarahmed9113@gmail.com>
2022-09-04 22:54:02 +0200

    update my repo

--- scripts/browser.sh
+++ scripts/browser.sh
@@ -2,14 +2,9 @@
 
 declare options=("Firefox
 Brave
-Google-chrome
-Chromium
-Qutebrowser
-librewolf")
+Google-chrome")
 
-#choice=$(echo -e "${options[@]}" | dmenu -l 10 -i -fn 'Roboto Mono' -p 'launch browser: ' -nb '#3b3b3b' -nf '#71988a' -sb '#000000' -sf '#71988a')
 choice=$(echo -e "${options[@]}" | dmenu -i -l 20 -g 2 -X 0 -Y 0 -W 680 -p 'launch browser: ' )
-#choice=$(echo -e "${options[@]}" | dmenu -i -l 20 -p 'launch browser: ' )
 
 case "$choice" in 
     quit)
@@ -23,15 +18,6 @@ case "$choice" in
     ;;
     Google-chrome)
         choice="google-chrome-stable"
-    ;;
-    Chromium)
-        choice="chromium"
-    ;;
-    Qutebrowser)
-        choice="qutebrowser"
-    ;;
-    librewolf)
-        choice="librewolf"
     ;;
 esac
 "$choice"
