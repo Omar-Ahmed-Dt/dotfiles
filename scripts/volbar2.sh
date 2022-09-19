12c4ad5a18ff31bf18ee2ae3f0bc79908fd699bc
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2022-09-19 23:51:42 +0200

    Update my config

--- scripts/volbar2.sh
+++ scripts/volbar2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-case $BLOCK_BUTTON in
-	1) amixer -q set Master toggle && pkill -RTMIN+10 i3blocks ;;  # left click
-	3) pavucontrol ;;  # right click
-#	2) whatever_you_want_to_run ;;  # middle click
-	4) amixer -q set Master 5%+ && pkill -RTMIN+10 i3blocks ;;  # scroll up
-	5) amixer -q set Master 5%- && pkill -RTMIN+10 i3blocks ;;  # scroll down
-esac
+#case $BLOCK_BUTTON in
+#	1) amixer -q set Master toggle && pkill -RTMIN+10 i3blocks ;;  # left click
+#	3) pavucontrol ;;  # right click
+##	2) whatever_you_want_to_run ;;  # middle click
+#	4) amixer -q set Master 5%+ && pkill -RTMIN+10 i3blocks ;;  # scroll up
+#	5) amixer -q set Master 5%- && pkill -RTMIN+10 i3blocks ;;  # scroll down
+#esac
 
 
 vol="$(ponymix | grep sink -A 2 | grep Volume | cut -c 16-)"
