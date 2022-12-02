eb5af5a63c04e8f395a84780620318ff79a33b60
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2022-12-02 22:27:02 +0200

    Update config

---
+++ scripts/lock.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+# i3lock --verif-text="Hello.." -ki ~/wallpapers/L98ctOs.png --bar-indicator --time-str="%I : %M %p" --date-str="%a, %B %d" -c 000000
+i3lock --verif-text="Hello.." -k --bar-indicator --time-str="%I : %M %p" --date-str="%a, %B %d" -c 000000 --time-color=bbc2cf --date-color=bbc2cf --time-size=46 --date-size=16 
