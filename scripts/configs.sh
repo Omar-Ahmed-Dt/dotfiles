a96039c61d7e4acb4238fdf2ef0d754b1b4cc3fe
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2022-12-16 14:38:03 +0200

    add swallow option to imgview for nnn plugins

---
+++ scripts/configs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+DMEDITOR="lvim"
+
+actions=(configs bash fish i3 i3_blocks mpv_input mpv_config kitty sxiv zathura ncmpcpp ncmpcpp_bindings sxhkd mimeapps lvim)
+
+selected=$(printf '%s\n' "${actions[@]}" | dmenu -i -l 20 -p "Edit Configs: ")
+
+case "$selected" in
+	configs)           $DMEDITOR $HOME/scripts/configs.sh ;;
+	bash)              $DMEDITOR $HOME/.bashrc ;;
+	fish)              $DMEDITOR $HOME/.config/fish/config.fish ;;
+	i3)                $DMEDITOR $HOME/.config/i3/config ;;
+	i3_blocks)         $DMEDITOR $HOME/.config/i3/i3blocks ;;
+	mpv_input)         $DMEDITOR $HOME/.config/mpv/input.conf ;;
+	mpv_config)        $DMEDITOR $HOME/.config/mpv/mpv.conf ;;
+	kitty)             $DMEDITOR $HOME/.config/kitty/kitty.conf ;;
+    sxiv)              $DMEDITOR $HOME/.config/sxiv/exec/key-handler ;;
+    zathura)           $DMEDITOR $HOME/.config/zathura/zathurarc ;;
+    ncmpcpp)           $DMEDITOR $HOME/.config/ncmpcpp/config ;;
+    ncmpcpp_bindings)  $DMEDITOR $HOME/.config/.config/ncmpcpp/ncmpcpp_bindings ;;
+    sxhkd)             $DMEDITOR $HOME/.config/sxhkd/sxhkdrc ;;
+    mimeapps)          $DMEDITOR $HOME/.config/mimeapps.list ;;
+    lvim)              $DMEDITOR $HOME/.config/lvim/config.lua ;;
+esac
+
