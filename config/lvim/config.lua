60e8490683cd1718c0686ba2e40cd55118d80523
Omar_Jupiter <omarahmed9113@gmail.com>
2023-01-30 23:59:42 +0200

    update config

--- config/lvim/config.lua
+++ config/lvim/config.lua
@@ -136,18 +136,35 @@ lvim.builtin.terminal.active = true
 
 lvim.plugins = {
 
+    {
+        "norcalli/nvim-colorizer.lua",
+        config = function()
+            require("colorizer").setup({ "css", "scss", "html", "javascript" }, {
+                RGB = true, -- #RGB hex codes
+                RRGGBB = true, -- #RRGGBB hex codes
+                RRGGBBAA = true, -- #RRGGBBAA hex codes
+                rgb_fn = true, -- CSS rgb() and rgba() functions
+                hsl_fn = true, -- CSS hsl() and hsla() functions
+                css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+                css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+            })
+        end,
+    },
+
+
     -- For markdown-preview
     --
     --
 
-    -- {
-    --     "iamcco/markdown-preview.nvim",
-    --     run = "cd app && npm install",
-    --     ft = "markdown",
-    --     config = function()
-    --         vim.g.mkdp_auto_start = 1
-    --     end,
-    -- },
+    {
+        "iamcco/markdown-preview.nvim",
+        -- run = "cd app && npm install",
+        build = "cd app && npm install",
+        ft = "markdown",
+        config = function()
+            vim.g.mkdp_auto_start = 1
+        end,
+    },
     --
     -- You must install glow globally
     -- https://github.com/charmbracelet/glow
