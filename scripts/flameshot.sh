45232d83293142e47931b3f91281175e2a35d108
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2023-03-03 01:43:03 +0200

    added flameshot.sh

--- scripts/flameshot.sh
+++ scripts/flameshot.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 #
-OPT=$(echo -e "A_clip\nA_save\nA_both\nW_clip\nW_save\nFull_clip\nFull_save\nFull_both" | dmenu -p "Choose action: ")
+OPT=$(echo -e "A_clip\nA_url\nA_save\nW_clip_save\nW_clip\nW_url\nW_save\nW_clip_save\nFull_clip\nFull_url\nFull_save\nFull_clip_save" | dmenu -p "Choose action: ")
 case $OPT in
 	A_clip ) flameshot gui -c  ;;
+	A_url )  flameshot gui -u ;; 
 	A_save)   flameshot gui -p ~/screenshots/  ;;
-	A_both ) flameshot gui -c -p ~/screenshots/ ;;
-	W_clip ) maim -i $(xdotool getactivewindow) | xclip -selection clipboard -i -t image/png   && notify-send -i ~/logo/screen.png "Screenshot taken" ;;
-    W_save) maim -i $(xdotool getactivewindow) ~/screenshots/$(date +'%Y%m%d-%H%M%S').png && notify-send -i ~/logo/screen.png "Screenshot taken" ;;
+	A_clip_save ) flameshot gui -c -p ~/screenshots/ ;;
+	W_clip ) flameshot gui --region $(slop -f "%wx%h+%x+%y") -c ;; 
+	W_url ) flameshot gui --region $(slop -f "%wx%h+%x+%y") -u ;; 
+    W_save)  flameshot gui --region $(slop -f "%wx%h+%x+%y") -p ~/screenshots/ ;; 
+    W_clip_save )  flameshot gui --region $(slop -f "%wx%h+%x+%y") -c -p ~/screenshots/ ;; 
     Full_clip )   flameshot full -c ;; 
+    Full_url )   flameshot full -u ;; 
     Full_save ) flameshot full -p ~/screenshots/ ;;
-    Full_both )  flameshot full -c -p ~/screenshots/ ;;
+    Full_clip_save )  flameshot full -c -p ~/screenshots/ ;;
 	*) ;;
 esac 
