69acdaf28e1464efe44eddb2f6df24a4b5909c05
Omar_Jupiter <omarahmed9113@gmail.com>
2023-12-08 01:39:04 +0200

    add wacom service

--- scripts/wacom.sh
+++ scripts/wacom.sh
@@ -8,7 +8,8 @@ for i in $(seq 10); do
     sleep 1
 done
 
-# /usr/local/bin/keys.sh
+
+/usr/local/bin/keys.sh
 
 
 # xsetwacom list devices
@@ -20,10 +21,18 @@ stylus_index=$(xsetwacom list devices | grep STYLUS | awk '{printf $7}')
 # set first left button for wacom pad as c+z
 xsetwacom set "${pad_index}" Button 1 "key +ctrl z -ctrl"
 
-# ratio with p3-onenote
+# select pin
+xsetwacom set "${pad_index}" Button 2 "key +ctrl 1"
+# select eraser
+xsetwacom set "${pad_index}" Button 3 "key +ctrl 4"
+xsetwacom set "${pad_index}" Button 8 "key +ctrl 5"
+
+# 
+# set ratio for rnote program: 
 # slop -f "%wx%h+%x+%y"
-xsetwacom set "${stylus_index}" MapToOutput 1415x853+465+194
+xsetwacom set "${stylus_index}" MapToOutput 1893x987+15+83
 
 # Get device name
-# name=$(lsusb | awk '/Wacom/ {print}' | cut -f7- -d' ')
-# notify-send "🎨 $name"
+name=$(lsusb | awk '/Wacom/ {print}' | cut -f7- -d' ')
+# notify-send "🎨 Device: <b>${name}</b> is connected"
+sudo -u omar DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus dunstify "Wacom Table is Plugged" -t 1500 -i /home/omar/logo/theme.png
