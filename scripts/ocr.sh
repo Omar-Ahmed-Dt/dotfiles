369b3eede5a21cf1a9efaa687d2d904d1219b217
Omar_Jupiter <omarahmed9113@gmail.com>
2024-04-17 20:42:11 +0200

    added ocr

--- scripts/ocr.sh
+++ scripts/ocr.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 #!/usr/bin/bash
 
-LANGUAGE=$(printf "eng\nara" | dmenu -i -p "Choose language: ")
-
-# maim -us ~/.cache/ocr/pic.png
-# tesseract -l $LANGUAGE  ~/.cache/ocr/pic.png ~/.cache/ocr/out
-# maim -us ~/.cache/ocr/pic.png && tesseract -l ara  ~/.cache/ocr/pic.png ~/.cache/ocr/out && xclip -sel clip ~/.cache/ocr/out.txt && notify-send -t 1500 'text copied to clipboard'
-maim -us ~/.cache/ocr/pic.png && tesseract -l $LANGUAGE  ~/.cache/ocr/pic.png ~/.cache/ocr/out && xclip -sel clip ~/.cache/ocr/out.txt && notify-send -t 1500 'text copied to clipboard'
+LANGUAGE=$(printf "eng\nara" | dmenu -i -p "Choose language: ") && maim -us ~/.cache/ocr/pic.png && tesseract -l $LANGUAGE  ~/.cache/ocr/pic.png ~/.cache/ocr/out && xclip -sel clip ~/.cache/ocr/out.txt && notify-send -t 1500 'text copied to clipboard'
