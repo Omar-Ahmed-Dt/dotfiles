84f8976e5daa7c17ff00db0d0fc05ee29c461bf6
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2024-12-21 16:50:49 +0200

    created currently playing script to manage mpd with i3blocks

--- scripts/clock.sh
+++ scripts/clock.sh
@@ -1,13 +1,26 @@
-#!/bin/bash
-#date '+%Y %b %d (%a) %I:%M%p'
-#date '+%d-%m-%y (%a) %I:%M%p'
-#date '+%a %d-%m'
+#!/bin/sh
+case "$BLOCK_BUTTON" in
+    1) kitty -e khal --color interactive ;;
+    3) kitty $HOME ;;
+esac
 
-date +"%a, %B %d"| sed 's/  / /g'
+clock=$(date '+%I')
 
-#case $BLOCK_BUTTON in
-#	1) pgrep -x dunst >/dev/null && notify-send "This Month" "$(cal --color=always | sedTERMINAL"s/..7m/<b><span color=\"red\">/;s/..27m/<\/span><\/b>/")" && notify-send "Appointments" "$(calcurse -D ~/.config/calcurse -d7)" ;;
-#	2) setsid -f kitty -e calcurse -D ~/.config/calcurse ;;
-#	3) pgrep -x dunst >/dev/null && notify-send " Time/date module" "\- Left click to show upcoming appointments for the next three days via \`calcurse -d3\` and show the month via \`cal\`
-#- Middle click opens calcurse if installed" ;;
-#esac
+case "$clock" in
+    "00") icon="N" ;;  # N for zero (nulla in Latin)
+    "01") icon="I" ;;  # One
+    "02") icon="II" ;; # Two
+    "03") icon="III" ;;# Three
+    "04") icon="IV" ;; # Four
+    "05") icon="V" ;;  # Five
+    "06") icon="VI" ;; # Six
+    "07") icon="VII" ;;# Seven
+    "08") icon="VIII";;# Eight
+    "09") icon="IX" ;; # Nine
+    "10") icon="X" ;;  # Ten
+    "11") icon="XI" ;; # Eleven
+    "12") icon="XII" ;;# Twelve
+esac
+
+echo "$(date '+%a.%d.%b') $icon $(date '+%I:%M %p')"
+
--- scripts/mediaplayer.sh
+++ scripts/mediaplayer.sh
@@ -1,156 +1,49 @@
 #!/usr/bin/env perl
-# Copyright (C) 2014 Tony Crisci <tony@dubstepdish.com>
-# Copyright (C) 2015 Thiago Perrotta <perrotta dot thiago at poli dot ufrj dot br>
+# Simplified MPD script to handle play/pause and display metadata
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+use strict;
+use warnings;
+use Env qw(BLOCK_BUTTON);
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-# Requires playerctl binary to be in your path (except cmus)
-# See: https://github.com/acrisci/playerctl
-
-# Set instance=NAME in the i3blocks configuration to specify a music player
-# (playerctl will attempt to connect to org.mpris.MediaPlayer2.[NAME] on your
-# DBus session).
-
-use Time::HiRes qw(usleep);
-use Env qw(BLOCK_INSTANCE);
-
-use constant DELAY => 50; # Delay in ms to let network-based players (spotify) reflect new data.
-use constant SPOTIFY_STR => 'spotify';
-
-my @metadata = ();
-my $player_arg = "";
-
-if ($BLOCK_INSTANCE) {
-    $player_arg = "--player='$BLOCK_INSTANCE'";
-}
-
+# Handle button presses for MPD
 sub buttons {
-    my $method = shift;
-
-    if($method eq 'mpd') {
+    if (defined $ENV{'BLOCK_BUTTON'}) {
         if ($ENV{'BLOCK_BUTTON'} == 1) {
-            system("mpc prev");
-        } elsif ($ENV{'BLOCK_BUTTON'} == 2) {
             system("mpc toggle");
         } elsif ($ENV{'BLOCK_BUTTON'} == 3) {
-            system("mpc next");
-        } elsif ($ENV{'BLOCK_BUTTON'} == 4) {
-            system("mpc volume +10");
-        } elsif ($ENV{'BLOCK_BUTTON'} == 5) {
-            system("mpc volume -10");
-        }
-    } elsif ($method eq 'cmus') {
-        if ($ENV{'BLOCK_BUTTON'} == 1) {
-            system("cmus-remote --prev");
-        } elsif ($ENV{'BLOCK_BUTTON'} == 2) {
-            system("cmus-remote --pause");
-        } elsif ($ENV{'BLOCK_BUTTON'} == 3) {
-            system("cmus-remote --next");
-        }
-    } elsif ($method eq 'playerctl') {
-        if ($ENV{'BLOCK_BUTTON'} == 1) {
-            system("playerctl $player_arg previous");
-            usleep(DELAY * 1000) if $BLOCK_INSTANCE eq SPOTIFY_STR;
-        } elsif ($ENV{'BLOCK_BUTTON'} == 2) {
-            system("playerctl $player_arg play-pause");
-        } elsif ($ENV{'BLOCK_BUTTON'} == 3) {
-            system("playerctl $player_arg next");
-            usleep(DELAY * 1000) if $BLOCK_INSTANCE eq SPOTIFY_STR;
-        } elsif ($ENV{'BLOCK_BUTTON'} == 4) {
-            system("playerctl $player_arg volume 0.01+");
-        } elsif ($ENV{'BLOCK_BUTTON'} == 5) {
-            system("playerctl $player_arg volume 0.01-");
-        }
-    } elsif ($method eq 'rhythmbox') {
-        if ($ENV{'BLOCK_BUTTON'} == 1) {
-            system("rhythmbox-client --previous");
-        } elsif ($ENV{'BLOCK_BUTTON'} == 2) {
-            system("rhythmbox-client --play-pause");
-        } elsif ($ENV{'BLOCK_BUTTON'} == 3) {
-            system("rhythmbox-client --next");
+            system("mpc prev");
         }
     }
 }
 
-sub cmus {
-    my @cmus = split /^/, qx(cmus-remote -Q);
-    if ($? == 0) {
-        foreach my $line (@cmus) {
-            my @data = split /\s/, $line;
-            if (shift @data eq 'tag') {
-                my $key = shift @data;
-                my $value = join ' ', @data;
-
-                @metadata[0] = $value if $key eq 'artist';
-                @metadata[1] = $value if $key eq 'title';
-            }
-        }
-
-        if (@metadata) {
-            buttons('cmus');
-
-            # metadata found so we are done
-            print(join ' - ', @metadata);
-            exit 0;
-        }
-    }
-}
-
+# Fetch MPD metadata
 sub mpd {
-    my $data = qx(mpc current);
-    if (not $data eq '') {
-        buttons("mpd");
-        print($data);
+    # Get MPD status and metadata
+    my $status_line = qx(mpc | grep -E '^\[playing\]|\[paused\]');
+    my $song = qx(mpc current);
+    chomp($status_line);
+    chomp($song);
+
+    # Extract the status (playing or paused)
+    my ($status) = $status_line =~ /\[(playing|paused)\]/;
+
+    # Check if song is playing or paused
+    if ($song eq '') {
+        print("No music playing\n");
+        exit 0;
+    } else {
+        buttons();
+
+        # Append "(Paused)" if the status is paused
+        if (defined $status && $status eq 'paused') {
+            print("$song (Paused)\n");
+        } else {
+            print("$song\n");
+        }
         exit 0;
     }
 }
 
-sub playerctl {
-    buttons('playerctl');
+# Run the MPD function
+mpd;
 
-    my $artist = qx(playerctl $player_arg metadata artist);
-    chomp $artist;
-    # exit status will be nonzero when playerctl cannot find your player
-    exit(0) if $? || $artist eq '(null)';
-
-    push(@metadata, $artist) if $artist;
-
-    my $title = qx(playerctl $player_arg metadata title);
-    exit(0) if $? || $title eq '(null)';
-
-    push(@metadata, $title) if $title;
-
-    print(join(" - ", @metadata)) if @metadata;
-}
-
-sub rhythmbox {
-    buttons('rhythmbox');
-
-    my $data = qx(rhythmbox-client --print-playing --no-start);
-    print($data);
-}
-
-if ($player_arg eq '' or $player_arg =~ /mpd/) {
-    mpd;
-}
-elsif ($player_arg =~ /cmus/) {
-    cmus;
-}
-elsif ($player_arg =~ /rhythmbox/) {
-    rhythmbox;
-}
-else {
-    playerctl;
-}
-print("\n");
