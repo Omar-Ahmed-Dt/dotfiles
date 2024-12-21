e7f8a90963f7fd59d74495d4fa9adc80e1f014be
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2024-12-21 20:18:33 +0200

    upload dmenu scripts

--- scripts/battery_info.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-capacity=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | awk '/percentage/ {print $2}')
-echo "Battery: $capacity"
-
--- scripts/currently_playing.sh
+++ scripts/currently_playing.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
+
 case "$BLOCK_BUTTON" in
     1) mpc toggle ;;  # Left click to toggle play/pause
     3) mpc prev ;;    # Right click to play the previous song
 esac
 
-# Get the current song name
-song=$(mpc current)
+# Get the current song name, truncate to 50 characters, and remove & from the output:
+# song=$(mpc current | cut -c 1-50 | sed 's/&//g')
+song=$(mpc current | tr -d '&<>|-')
 
 # Extract the current/total timestamp using awk (handles both playing and paused)
 timestamps=$(mpc | grep -E "playing|paused" | awk '{print $3}')
 
 # Check if MPD is running and determine output
 if [ -z "$song" ] || [ -z "$timestamps" ]; then
-    echo "No music playing"
+    echo "No_music_playing"
+    echo "No_music_playing" > /tmp/i3blocks_music.log
 else
     if mpc | grep -q "paused"; then
-        echo "$song [$timestamps] (Paused)"
+        output="$song [$timestamps] (Paused)"
     else
-        echo "$song [$timestamps]"
+        output="$song [$timestamps]"
     fi
+    echo "$output"
 fi
