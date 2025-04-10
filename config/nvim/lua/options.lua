e1b446ff5e129af9465dcf03d38cd00ea43eeb96
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2025-04-10 02:43:29 +0200

    more customization for nvchad

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,4 +1,21 @@
 require "nvchad.options"
 local opt = vim.opt
-opt.number = true         -- show absolute line number on current line
-opt.relativenumber = true -- show relative numbers on other lines
+
+-- Horizontal and vertical crosshair
+opt.cursorline = true           -- Highlight the line
+opt.cursorcolumn = true         -- Highlight the column
+opt.cursorlineopt = 'number,line'
+vim.cmd [[
+  highlight CursorLine guibg=#2c2c2c
+  highlight CursorColumn guibg=#2c2c2c
+]]
+
+-- Optional: extra guide line for max width (like column 120)
+opt.colorcolumn = "120"
+
+-- Other good UX defaults
+opt.number = true
+opt.relativenumber = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
