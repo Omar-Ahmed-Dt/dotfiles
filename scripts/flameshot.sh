180a7c438436a7ca45df81533bf2555b28c5c93a
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2025-05-27 01:22:41 +0300

    change dmenu to center

--- scripts/flameshot.sh
+++ scripts/flameshot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
-OPT=$(echo -e "1_A_clip\n2_A_save\n3_A_clip_save\n4_W_clip\n5_W_save\n6_W_clip_save\n7_Full_clip\n22_Full_save\n33_Full_clip_save" | dmenu -i -p "Choose action: ")
+OPT=$(echo -e "1_A_clip\n2_A_save\n3_A_clip_save\n4_W_clip\n5_W_save\n6_W_clip_save\n7_Full_clip\n22_Full_save\n33_Full_clip_save" | dmenu -c -l 9 -i -p "Choose action: ")
 case $OPT in
 	1_A_clip ) flameshot gui -s -c  ;;
 	2_A_save)   flameshot gui -s  -p ~/screenshots/  ;;
