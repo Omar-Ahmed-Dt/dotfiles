151a66273ece3de7a5016d803225e2385b1d044a
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2025-05-27 00:42:53 +0300

    md power menu script and some changes

--- scripts/clock.sh
+++ scripts/clock.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 case "$BLOCK_BUTTON" in
-    1) kitty -e khal --color interactive ;;
-    3) kitty $HOME ;;
+    # 3) kitty -e khal --color interactive ;;
+    1) kitty $HOME ;;
 esac
 
 clock=$(date '+%I')
