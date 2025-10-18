f0f3e724f49be314a4361c68974810b99419181c
Omar-Ahmed-Dt <omarahmed9113@gmail.com>
2025-10-18 20:23:37 +0300

    added vol.sh

--- scripts/rip.sh
+++ scripts/rip.sh
@@ -1,16 +1,4 @@
 #!/bin/sh
 
-# dig +short myip.opendns.com @resolver1.opendns.com
-
 myip="$(dig +short myip.opendns.com @resolver1.opendns.com)"
 echo "My WAN/Public IP address: ${myip}"
-
-# ip=$(curl -s icanhazip.com);
-
-# if [[ "$BLOCK_BUTTON" -eq 1 ]];
-# then
-# 	netname=$(whois $ip | grep '^netname:' | awk '{print $2}' | tail -1);
-# 	echo "$netname"
-# else
-# 	echo "$ip"
-# fi
